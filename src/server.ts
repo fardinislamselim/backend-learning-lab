10b57410dd815814bade78a8f6017df5c975c375
fardinislamselim <codewithfardin@gmail.com>
2026-05-16 13:10:54 +0600

    integrate dotenv and pg for environment configuration and database connection

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,53 @@
-import express, { type Application, type Request, type Response } from "express";
+import { config } from "dotenv";
+import express, {
+  type Application,
+  type Request,
+  type Response,
+} from "express";
+import pg, { Pool } from "pg";
 
-const app:Application = express();
+config();
+
+const app: Application = express();
 const port = 5000;
 
-app.use(express.json())
+app.use(express.json());
 
-app.get("/", (req:Request, res:Response) => {
-  res.status(200).json({ message: "Hello World!"});
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({ message: "Hello World!" });
 });
 
-app.post("/", async (req: Request, res: Response) => { 
-  console.log(req.body);
-})
+const initDB = async () => {
+  try {
+    await pool.query(`
+    CREATE TABLE IF NOT EXISTS users (
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(20) ,
+      email VARCHAR(20) NOT NULL,
+      password VARCHAR(20) NOT NULL,
+      is_active BOOLEAN DEFAULT true,
+      age INT,
+
+      created_at TIMESTAMP DEFAULT NOW(),
+      updated_at TIMESTAMP DEFAULT NOW()
+    );
+  `);
+  } catch (error) {
+    console.log(`DB connection Error ${error}`);
+  }
+};
+
+initDB()
+
+app.post("/", async (req: Request, res: Response) => {
+  const { name, email, password } = req.body;
+  res.status(201).json({
+    message: "Created!",
+    data: { name, email },
+  });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
