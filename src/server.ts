103dfac603b5c2f0013219d9006fef6891455f56
fardinislamselim <codewithfardin@gmail.com>
2026-05-16 10:31:36 +0600

    enhance server setup with TypeScript types and JSON response

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,17 @@
-import express, { type Request, type Response } from "express";
-const app = express();
+import express, { type Application, type Request, type Response } from "express";
+
+const app:Application = express();
 const port = 5000;
 
+app.use(express.json())
+
 app.get("/", (req:Request, res:Response) => {
-  res.send("Hello World!");
+  res.status(200).json({ message: "Hello World!"});
 });
+
+app.post("/", async (req: Request, res: Response) => { 
+  console.log(req.body);
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
